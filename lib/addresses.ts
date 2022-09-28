e01a2aef385916799197b7c32a80399ad4d4eb2d
Noam <noamru22@gmail.com>
2022-09-28 12:25:38 +0200

    pay in USDC

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -3,3 +3,6 @@ import { PublicKey } from '@solana/web3.js'
 export const shopAddress = new PublicKey(
   'A4c21HFs9doq5Pmu36NFhYcmfhG4aYwxoyoVb9ETurEY'
 )
+export const usdcAddress = new PublicKey(
+  'Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr'
+)
